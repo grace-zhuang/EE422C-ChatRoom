2fd7eb6372a7dfc3cc302b0d207c2149ba195d4e
grace-zhuang <grace.zhuang@utexas.edu>
2017-04-23 20:33:12 -0500

    "Starter Code"

--- chatClient.java
+++ chatClient.java
@@ -1,5 +1,89 @@
+/* CHAT ROOM chatClient.java
+ * EE422C Project 7 submission by
+ * Samuel Zhang
+ * shz96
+ * 16225
+ * Grace Zhuang
+ * gpz68
+ * 16215
+ * Slip days used: <0>
+ * Spring 2017
+ */
+
 package assignment7;
 
-public class chatClient {
+import java.io.*; 
+import java.util.Observable;
+import java.util.Observer;
 
+public class chatClient {
+	
+	private String name;
+	// JavaFX
+	private BufferedReader reader;
+	private PrintWriter writer;
+	
+	public void run() {
+		initFX();
+		setUpNetworking();
+	}
+	
+	public void initFX() {
+		// Java FX
+	}
+	
+	public void setUpNetworking() {
+		// set up network connections
+	}
+	
+	public void newChat() {
+		// create new JavaFX chat window
+	}
+	
+	public void startChat() {
+		newChat();
+		// add protocol and send to writer
+		// receive back ID
+	}
+	
+	public class chatWindow {
+		private int ID;
+		// JavaFX
+		
+		public chatWindow(int num) {
+			ID = num;
+		}
+		
+		public int getID() {
+			return ID;
+		}
+		
+		public void updateChat() {
+			// parse message, update chat
+		}
+		
+		public void sendMessage() {
+			// add protocol, send message
+		}
+		
+	}
+	
+	public class clientObserver implements Observer {
+
+		@Override
+		public void update(Observable arg0, Object arg1) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	public class incomingReader implements Runnable {
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			
+		}
+	}
 }
--- chatServer.java
+++ chatServer.java
@@ -1,5 +1,49 @@
+/* CHAT ROOM chatServer.java
+ * EE422C Project 7 submission by
+ * Samuel Zhang
+ * shz96
+ * 16225
+ * Grace Zhuang
+ * gpz68
+ * 16215
+ * Slip days used: <0>
+ * Spring 2017
+ */
+
 package assignment7;
 
-public class chatServer {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Observable;
 
+import assignment7.chatClient.clientObserver;
+
+public class chatServer {
+	private ArrayList<chatRoom> openChats;
+	private HashMap<String, clientObserver> info;
+	
+	public void setUpNetworking() {
+		
+	}
+	
+	public static void main(String[] args) {
+		try {
+		new chatServer().setUpNetworking();
+		} catch (Exception e) { e.printStackTrace(); }
+	}
+	
+	public class chatRoom extends Observable {
+		private int ID;
+		
+		public chatRoom(int num) {
+			ID = num;
+		}	
+	}
+	
+	public class clientHandler implements Runnable {
+		public void run() {
+			
+		}
+	}
+	
 }
