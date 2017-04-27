1897f61d9df717a2bdd00e47b66330fb38292df8
Samuel Zhang <zhangheavens@gmail.com>
2017-04-27 03:15:59 -0500

    online users is working
    added javafx thread to make it work

--- ChatClient.java
+++ ChatClient.java
@@ -12,13 +12,14 @@
 
 package assignment7;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.Observable;
-import java.util.Observer;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
@@ -33,26 +34,26 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class ChatClient extends Application {
-	
+
 	private String name;
 	private BufferedReader reader;
 	private PrintWriter writer;
 	private static final String separator = Character.toString((char) 31);
 	private static final String nameSeparator = Character.toString((char) 29);
 	private Stage home = null;
-	
-	
+
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		setUpNetworking();
 		home = new Stage();
-		
+
 		primaryStage.setTitle("Chat Room");
 		VBox open = new VBox();
-		
+
 		Label title = new Label("Welcome to Chat Room!");
 		title.setFont(Font.font("Bradley Hand ITC", 30));
-		
+
 		Label instruction = new Label("Please enter a username:");
 		TextField username = new TextField();
 		username.setPromptText("Username");
@@ -64,32 +65,32 @@ public class ChatClient extends Application {
 				String s = username.getText();
 				if (s != null) { //FIX THIS!!!!!
 					name = s;
-					
+
 					writer.println("NEWUSER" + separator + s);
 					writer.flush();
-					
-					
+
+
 					writer.println("GETONLINE" + separator + name);
 					writer.flush();
-				
+
 					primaryStage.close();
 				}	
 			}
-			
+
 		});
-		
+
 		open.getChildren().addAll(title, instruction, username, done);
-		
+
 		Scene scene = new Scene(open, 300, 300);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
-	
+
 	private void setUpNetworking() throws Exception {
 		System.out.println("Setting up networking");
 
 		@SuppressWarnings("resource") 
-		
+
 		Socket sock = new Socket("127.0.0.1", 4242);
 		InputStreamReader streamReader = new InputStreamReader(sock.getInputStream());
 		reader = new BufferedReader(streamReader);
@@ -105,42 +106,42 @@ public class ChatClient extends Application {
 		Thread readerThread = new Thread(new IncomingReader()); 
 		readerThread.start();
 	}
-	
+
 	public void loggedIn(String[] available) {
 		VBox chat = new VBox();
 		Label welcome = new Label("Welcome, " + name + "!");
-							
+
 		Label people = new Label("Who would you like to chat with?");
-		
+
 		CheckBox[] online = new CheckBox[available.length];
 		for (int i = 0; i < available.length; i++) {
 			online[i] = new CheckBox(available[i]);
 			chat.getChildren().add(online[i]);
 		}
-		
-		
+
+
 		chat.getChildren().addAll(welcome, people);
 		Scene realScene = new Scene(chat, 300, 300);
 		home.setScene(realScene);
 		home.show();
 	}
-	
+
 
 	public void startChat() {
 		// writer.println("NEWCHAT" + separator + name + );
 		// receive back ID
 	}
-	
+
 	class ChatWindow {
-		
+
 		private int ID;
-		
+
 		public ChatWindow(int num) {
 			ID = num;
-			
+
 			Stage chat = new Stage();
 			chat.setTitle("Chat Window");
-			
+
 			ScrollPane convo = new ScrollPane();
 			convo.setVbarPolicy(ScrollBarPolicy.ALWAYS);
 			TextField text = new TextField();
@@ -155,7 +156,7 @@ public class ChatClient extends Application {
 						sendMessage(s);
 					}
 				}
-				
+
 			});
 			VBox box = new VBox();
 			box.getChildren().addAll(convo, text, send);
@@ -163,23 +164,23 @@ public class ChatClient extends Application {
 			chat.setScene(scene);
 			chat.show();
 		}
-		
+
 		public int getID() {
 			return ID;
 		}
-		
+
 		public void updateChat() {
 			// parse message, update chat
 		}
-		
+
 		public void sendMessage(String message) {
 			writer.println(ID + separator + name + separator + message);
 			writer.flush();
 		}
-		
+
 
 	}
-	
+
 	class IncomingReader implements Runnable {
 
 		@Override
@@ -187,21 +188,30 @@ public class ChatClient extends Application {
 			String incoming;
 			try {
 				while ((incoming = reader.readLine()) != null) {
-					
+
 					String[] message = incoming.split(separator);
 					if (message[0].equals("GETONLINE")) {
-						loggedIn(message[2].split(nameSeparator));
+
+
+						Platform.runLater(new Runnable() {
+
+							@Override
+							public void run() { 
+								loggedIn(message[2].split(nameSeparator));
+							}
+						});
+
 					}
-				
+
 				}
 			} catch (IOException ex) { ex.printStackTrace(); }			
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		try {
 			launch(args);
 		} catch (Exception e) { e.printStackTrace(); }
 	}
-		
+
 }
